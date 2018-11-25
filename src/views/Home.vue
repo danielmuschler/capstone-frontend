2970f16500222bed87836cfed99a6039a2720546
danielmuschler <dmusch4@gmail.com>
2018-11-25 14:35:17 -0600

    added route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="pitch in pitches"></div>
+<!--     <h2>{{pitches.title}}</h2>
+    <h2>{{pitches.locations}}</h2> -->
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
+var axios = require("axios");
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      pitches: [],
     };
   },
-  created: function() {},
+
+  created: function() {
+    axios.get("http://localhost:3000/api/pitches").then(function(response) {
+      console.log(response.data);
+      this.pitches = response.data;
+    }.bind(this)
+    );
+  },
   methods: {},
   computed: {}
 };

ea838873ee0a864064c18e894f7c8dc7410d450b
danielmuschler <dmusch4@gmail.com>
2018-11-28 20:23:50 -0600

    all connections between front and back end for save to database made

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -26,28 +26,40 @@
         <div class="form-group">
           <label>Filmmaker Bio:</label> <input type="text" class="form-control" v-model="filmmaker_bio" />
         </div>
-        <h3>Characters</h3>
-        <div class="form-group">
-          <label>First Name:</label> <input type="text" class="form-control" v-model="first_name" />
+
+        <div>
+          <h3>Characters</h3>
+          <div class="form-group">
+            <label>First Name:</label> <input type="text" class="form-control" v-model="first_name" />
+          </div>
+          <div class="form-group">
+            <label>Last Name:</label> <input type="text" class="form-control" v-model="last_name" />
+          </div>
+          <div class="form-group">
+            <label>Description:</label> <input type="text" class="form-control" v-model="character_description" />
+          </div>
         </div>
-        <div class="form-group">
-          <label>Last Name:</label> <input type="text" class="form-control" v-model="last_name" />
+
+        <div>
+          <h3>Locations</h3>
+          <div class="form-group">
+            <label>Name:</label> <input type="text" class="form-control" v-model="location_name" />
+          </div>
+          <div class="form-group">
+            <label>Description:</label> <input type="text" class="form-control" v-model="location_description" />
+          </div>
         </div>
-        <div class="form-group">
-          <label>Description:</label> <input type="text" class="form-control" v-model="description" />
-        </div>
-        <h3>Locations</h3>
-        <div class="form-group"><label>Name:</label> <input type="text" class="form-control" v-model="name" /></div>
-        <div class="form-group">
-          <label>Description:</label> <input type="text" class="form-control" v-model="description" />
-        </div>
-        <div class="form-group">
+
+        <div>
           <h3>Music</h3>
-          <label>Song Name:</label> <input type="text" class="form-control" v-model="name" />
+          <div class="form-group">
+            <label>Song Name:</label> <input type="text" class="form-control" v-model="song_name" />
+          </div>
+          <label>Artist:</label> <input type="text" class="form-control" v-model="artist" />
+          <label>Description:</label> <input type="text" class="form-control" v-model="song_description" />
+          <div class="form-group"></div>
         </div>
-        <label>Artist:</label> <input type="text" class="form-control" v-model="artist" /> <label>Description:</label>
-        <input type="text" class="form-control" v-model="description" />
-        <div class="form-group"></div>
+
         <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
     </div>
@@ -61,28 +73,52 @@ export default {
   template: "#login-page",
   data: function() {
     return {
-      name: "",
-      password: "",
+      title: "",
+      logline: "",
+      genre: "",
+      synopsis: "",
+      producer_statement: "",
+      thematic_description: "",
+      visual_style_description: "",
+      filmmaker_bio: "",
+      first_name: "",
+      last_name: "",
+      character_description: "",
+      location_name: "",
+      location_description: "",
+      song_name: "",
+      artist: "",
+      song_description: "",
       errors: []
     };
   },
   methods: {
     submit: function() {
       var params = {
-        name: this.name,
-        password: this.password
+        title: this.title,
+        logline: this.logline,
+        genre: this.genre,
+        synopsis: this.synopsis,
+        producer_statement: this.producer_statement,
+        thematic_description: this.thematic_description,
+        visual_style_description: this.visual_style_description,
+        filmmaker_bio: this.filmmaker_bio,
+        location_name: this.location_name,
+        location_description: this.location_description,
+        first_name: this.first_name,
+        last_name: this.last_name,
+        character_description: this.character_description,
+        song_name: this.song_name,
+        artist: this.artist,
+        song_description: this.song_description
       };
       axios
         .post("http://localhost:3000/api/pitches", params)
         .then(response => {
-          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
-          localStorage.setItem("jwt", response.data.jwt);
           this.$router.push("/");
         })
         .catch(error => {
-          this.errors = ["Invalid email or password."];
-          this.email = "";
-          this.password = "";
+          this.errors = error.response.data.errors;
         });
     }
   }
